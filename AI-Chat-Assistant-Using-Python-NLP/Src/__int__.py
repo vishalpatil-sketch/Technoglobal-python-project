20b471d5cca97f6f5fd1b269d0d2bda0ae78ef39
Vishal Patil <vishalpatil122004@gmail.com>
2026-09-19 11:05:45 +0530

    Create __init__.py for AI Chat Assistant package

---
+++ AI-Chat-Assistant-Using-Python-NLP/Src/__int__.py
@@ -0,0 +1,9 @@
+"""
+AI Chat Assistant Package
+"""
+
+from src.preprocessor import TextPreprocessor
+from src.model import ChatbotModel
+from src.voice_assistant import VoiceAssistant
+
+__all__ = ["TextPreprocessor", "ChatbotModel", "VoiceAssistant"]
